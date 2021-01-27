a078707fb1c095f1d5476b3fe5cd857ecef0dfb1
Jacco Meijer <jacco@jaccomeijer.nl>
2021-01-27 13:44:56 +0100

    Implement set vars script

--- src/static/set-vars.ts
+++ src/static/set-vars.ts
@@ -1,4 +1,15 @@
 import { theme } from '../lib/theme'
+import { cssObjToVars } from '../lib/css-obj-to-vars'
 
-console.log('localStorage', localStorage.theme)
-Object.keys(theme).forEach((key) => console.log('Key', key))
+const themeIds = Object.keys(theme)
+const themeId = localStorage.theme || themeIds[0]
+
+if (!localStorage.theme) {
+  localStorage.theme = themeId
+}
+const cssVars = cssObjToVars({ obj: theme[themeId] })
+console.log(`Found themeId ${themeId}, setting css vars:`, cssVars)
+
+Object.entries(cssVars).forEach(([key, value]: any) =>
+  document.documentElement.style.setProperty(key, value)
+)
